a5168582075eaec2d9016886cd766c4a5109bf00
vermaanv01-boop <verma.anv01@gmail.com>
2026-03-20 22:52:31 +0530

    updated

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -3,63 +3,82 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { configDotenv } = require("dotenv");
 configDotenv();
+const User = require("../Models/userModel");
+const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/generateToken");
+
 const registerUser = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    if (!username || !email || !password) {
-      return res.status(400).json({
-        message: "All field are Required",
-      });
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
     }
 
-    const existingUser = await User.findOne({
-      $or: [{ email }, { username }],
-    });
+    const userExists = await User.findOne({ email });
 
-    if (existingUser) {
-      return res.status(400).json({
-        message: "User Already Registered",
-      });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-      username,
+      name,
       email,
-      password,
+      password: hashedPassword,
     });
-    return res.status(201).json({
-      message: "User Created successfully",
+
+    const token = generateToken(user);
+
+    res.status(201).json({
+      message: "User registered",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
     });
-  } catch (err) {
-    return res.status(500).json({
-      error: err.message,
-    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
-
 const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ message: "Invalid password" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expires: "7d",
-    });
-    res.json({
+
+    const token = generateToken(user);
+
+    res.status(200).json({
+      message: "Login successful",
       token,
-      user,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
     });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
--- Models/userModel.js
+++ Models/userModel.js
@@ -19,4 +19,15 @@ const userSchema = new mongoose.Schema({
 
 },{timestamps : true});
 
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
 module.exports = mongoose.model("User",userSchema);
