e523baa95203c99612a186288665edd811c2aabf
vermaanv01-boop <verma.anv01@gmail.com>
2026-03-21 22:44:25 +0530

    updated

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+app.use("/api/users",userRoutes)
+app.use("api/problems",problemRoter);
+app.use("/api/submission",submissionRoutes);
+
+
 
 app.listen(5000,()=>{
     console.log("Server started");
 });
-app.use("/api/users",userRoutes)
-app.use("api/problems",problemRoter);
-app.use("/api/submission",submissionRoutes);
 
