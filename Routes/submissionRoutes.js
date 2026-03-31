e5c39e95c6c586f4d381ed13d4e87aa9d3276207
vermaanv01-boop <verma.anv01@gmail.com>
2026-03-31 23:32:28 +0530

    updated

--- Routes/submissionRoutes.js
+++ Routes/submissionRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleWare/Auth");
-const { submitCode , getUserSubmissions } = require("../controllers/submissionController");
+const { submitCode , getUserSubmissions } = require("../Controllers/submissionController");
 
 router.post("/submit", protect, submitCode);
 router.get("/user/:userId",protect, getUserSubmissions);
